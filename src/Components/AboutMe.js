1f7946d463c26c74e3ce35c29fbf10f9d3f90e08
Abdulwajid Arikattayil <abdul@Abdulwajids-MacBook-Air.local>
2022-03-24 15:54:11 -0400

    added new components

---
+++ src/Components/AboutMe.js
@@ -0,0 +1,8 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+export default function Aboutme(){
+    return(
+        <h1>Blah Blah Blah</h1>
+    )
+}
---
+++ src/Components/Name.js
@@ -0,0 +1,8 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+export default function Name(){
+    return(
+        <h1>Abdul</h1>
+    )
+}
